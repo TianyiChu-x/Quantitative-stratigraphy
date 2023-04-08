b1ae92ff95c4d4dc29de0b7a8a8bfc82adfaa025
TianyiChu-x <130201525+TianyiChu-x@users.noreply.github.com>
2023-04-08 12:15:48 +0800

    Update main.py

--- HA/main.py
+++ HA/main.py
@@ -1,1 +1,21 @@
+import pandas as pd
+
+from HorizonAnneal import HorizonAnneal
+from penalty_spec__Riley_62_set import produce_list
+from timer import Timer
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    t = Timer(text="Total time: {:.5f} seconds")
+    t.start()
+    myCH = pd.read_csv("./dataset/Horizon_Data_srandom_for_HA.csv")
+    mySections = pd.read_csv("./dataset/riley_section_names.csv")
+    myCH = myCH.drop(["Section_No"], axis=1)
+    penalty_spec_62 = produce_list()
+    j = HorizonAnneal(myCH, pen_str=penalty_spec_62)
+    t.stop()
 
